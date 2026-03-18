5f8c482da930f68a62fa045ebfa93458e4d1c4dd
saroshkhann <ksarosh785@gmail.com>
2026-03-18 16:41:32 +0500

    OOP

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-cityName = input("What's the name of the city you grew up in?\n")
-petName = input("What's your pet's name?\n")
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-print("Your band name could be " + cityName + " " + petName)
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options =menu.get_items()
+    choice = input(f"What would you like? {options}")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+               coffee_maker.make_coffee(drink)
