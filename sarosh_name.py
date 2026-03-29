9d5afd17eebd66b131c6c7f2ca6226c0b1331484
saroshkhann <ksarosh785@gmail.com>
2026-03-29 18:52:21 +0500

    letter S

---
+++ sarosh_name.py
@@ -0,0 +1,18 @@
+import turtle as turtle_module
+
+sarosh = turtle_module.Turtle()
+
+sarosh.setheading(180)
+sarosh.forward(40)
+sarosh.left(90)
+sarosh.forward(40)
+sarosh.left(90)
+sarosh.forward(40)
+sarosh.setheading(270)
+sarosh.forward(40)
+sarosh.right(90)
+sarosh.forward(40)
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
