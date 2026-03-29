eabe5352aa21a4bbb1ed19259898d272e35ca5b0
saroshkhann <ksarosh785@gmail.com>
2026-03-29 15:57:11 +0500

    Turtle Graphics

---
+++ random_walk.py
@@ -0,0 +1,23 @@
+import turtle
+from turtle import Turtle
+import random
+
+tim = Turtle()
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color =  (r,g,b)
+    return random_color
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray","SeaGreen"]
+directions = [0,90,180,270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color((random_color()))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
